2b11ddcc9b4e36251dbffa5f349c94ccfe4afddd
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-16 01:08:36 +0600

    forms updated

--- musicians/form/musician.py
+++ musicians/form/musician.py
@@ -1,9 +1,22 @@
 from django import forms
 
+from musicians.models.album import Album
 from musicians.models.musician import Musician
 
 
 class MusicianForm(forms.ModelForm):
+    albums = forms.ModelMultipleChoiceField(
+        queryset=Album.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Musician
-        fields = ["first_name", "last_name", "email", "phone_number", "instrument_type"]
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "instrument_type",
+            "albums",
+        ]
